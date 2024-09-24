3e8a7fb710c703a798d097bb65ccb5a33d337529
Denilson <fcodenilson@gmail.com>
2024-09-24 07:35:47 -0300

    Refactor admin views and add list display, list editable, list filter, and search fields for Player, Agent, Map, Tier, and Stat models

--- gamedata/admin.py
+++ gamedata/admin.py
@@ -4,25 +4,100 @@ from .models import Agent, Role, Map, Tier
 
 @admin.register(Agent)
 class AgentAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {"fields": ("name", "role")}),
+        (
+            "Image URLs",
+            {
+                "fields": (
+                    "icon",
+                    "small_icon",
+                    "full_portrait",
+                    "background",
+                )
+            },
+        ),
+        ("Description", {"fields": ("description",)}),
+    )
+
     list_display = [
         "name",
+        "role",
+    ]
+
+    list_filter = [
+        "role",
+    ]
+
+    list_editable = [
         "role",
     ]
 
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
-    pass
+
+    fields = [
+        "name",
+        "uuid",
+        "description",
+        "icon",
+    ]
+
+    readonly_fields = [
+        "uuid",
+    ]
 
 
 @admin.register(Map)
 class MapAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("name", "uuid")}),
+        (
+            "Image URLs",
+            {
+                "fields": (
+                    "display_icon",
+                    "list_horizontal_icon",
+                    "list_vertical_icon",
+                    "splash",
+                )
+            },
+        ),
+    )
+
+    list_display = [
+        "name",
+        "uuid",
+    ]
+
+    readonly_fields = [
+        "uuid",
+    ]
 
 
 @admin.register(Tier)
 class TierAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {"fields": ("tier", "division")}),
+        (
+            "Image URLs",
+            {
+                "fields": (
+                    "small_icon",
+                    "large_icon",
+                )
+            },
+        ),
+    )
+
     list_display = [
         "tier",
+        "division",
+    ]
+
+    list_filter = [
         "division",
     ]
