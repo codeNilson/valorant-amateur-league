f890979433231cff93de1cd0012df86ded88db3d
Denilson <fcodenilson@gmail.com>
2024-10-11 13:51:30 -0300

    Refactor settings: Add ACCOUNT_SESSION_REMEMBER option for session remember functionality

--- players/forms/player_login_form.py
+++ players/forms/player_login_form.py
@@ -5,6 +5,5 @@ from utils.forms_utils import update_form_fields
 class PlayerLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        update_form_fields(self, "login", css_class="form-control", autofocus=True)
-        update_form_fields(self, "password", css_class="form-control")
-        update_form_fields(self, "remember", css_class="form-check-input")
+        update_form_fields(self, "login", css_class="form-control border-0 bg-transparent", autofocus=True)
+        update_form_fields(self, "password", css_class="form-control border-0 bg-transparent")
