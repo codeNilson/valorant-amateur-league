047af7cba7aaa5fc0b849d6333e423af06cc69a7
Denilson <fcodenilson@gmail.com>
2024-10-17 16:49:43 -0300

    Refactor CSS: Update login button color and class

--- lavava/settings/messages.py
+++ lavava/settings/messages.py
@@ -1,9 +1,9 @@
 from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
-    messages.DEBUG: "text-bg-info border-info",
-    messages.INFO: "text-bg-info border-info",
-    messages.SUCCESS: "text-bg-success border-success",
-    messages.WARNING: "text-bg-warning border-warning",
-    messages.ERROR: "text-bg-danger border-danger",
+    messages.DEBUG: "alert-info",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
 }
--- matches/views.py
+++ matches/views.py
@@ -1,9 +1,23 @@
+from django.db.models import Subquery, OuterRef
+from stats.models import Stat
+from matches.models import Match
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
 
 
 # Create your views here.
 class MatchHistory(View):
+
     def get(self, request):
-        return render(request, "matches/match_history.html")
+        # Subconsulta para obter o jogador MVP para cada partida
+        mvp_subquery = Stat.objects.filter(
+            team__match=OuterRef("pk"), mvp=True
+        ).values("player__username")[:1]
+
+        matches = Match.objects.prefetch_related("map", "winner", "teams__players__main_agent")
+        matches = matches.annotate(mvp=Subquery(mvp_subquery))
+
+        for match in matches:
+            print(match.mvp)
+
+        return render(request, "matches/match_history.html", {"matches": matches})
