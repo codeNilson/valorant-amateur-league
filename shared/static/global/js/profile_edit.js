e271cb6a5c9bb8d1764fd17e3331da2ee665043d
Denilson <fcodenilson@gmail.com>
2024-10-22 07:38:46 -0300

    Refactor profile edit form toggle functionality

--- shared/static/global/js/profile_edit.js
+++ shared/static/global/js/profile_edit.js
@@ -1,8 +1,28 @@
-button = document.getElementById("editButton");
-form = document.getElementById("editForm");
-playerInfo = document.getElementById("playerInfo");
+// button = document.getElementById("editButton");
+// form = document.getElementById("editForm");
+// playerInfo = document.getElementById("playerInfo");
 
-button.addEventListener("click", function () {
-    form.classList.toggle("d-none");
-    playerInfo.classList.toggle("d-none");
+// button.addEventListener("click", function () {
+//     form.classList.toggle("d-none");
+//     playerInfo.classList.toggle("d-none");
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    function toggleEditForm() {
+
+        button = document.getElementById("editButton");
+        form = document.getElementById("editForm");
+        playerInfo = document.getElementById("playerInfo");
+
+        if (button && form && playerInfo) {
+            button.addEventListener("click", function () {
+                form.classList.toggle("d-none");
+                playerInfo.classList.toggle("d-none");
+            });
+        } else {
+            console.error("One or more elements not found.");
+        }
+    }
+
+    toggleEditForm();
 });
