9088a2027a11fc1c5b6aee665e5a786f2d58fe16
Denilson <fcodenilson@gmail.com>
2024-10-27 11:55:39 -0300

    Refactor CSS and HTML: Hide YouTube captcha and update background size

--- players/static/players/js/profile_edit.js
+++ players/static/players/js/profile_edit.js
@@ -1,23 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     function toggleEditForm() {
-        const editButton = document.getElementById("editButton");
-        const cancelButton = document.getElementById("cancelButton");
-        const form = document.getElementById("editForm");
-        const playerInfo = document.getElementById("playerInfo");
+        const editButtons = document.querySelectorAll(".editButton");
+        const cancelButtons = document.querySelectorAll(".cancelButton");
 
-        if (editButton && form && playerInfo && cancelButton) {
-            editButton.addEventListener("click", function () {
-                form.classList.toggle("d-none");
-                playerInfo.classList.toggle("d-none");
-            });
+        editButtons.forEach(editButton => {
+            const dataId = editButton.getAttribute("data-id");
+            const form = document.querySelector(`.editForm[data-id="${dataId}"]`);
+            const playerInfo = document.querySelector(`.playerInfo[data-id="${dataId}"]`);
+            const cancelButton = document.querySelector(`.cancelButton[data-id="${dataId}"]`);
 
-            cancelButton.addEventListener("click", function () {
-                form.classList.add("d-none");
-                playerInfo.classList.remove("d-none");
-            });
-        } else {
-            console.error("One or more elements not found.");
-        }
+            if (form && playerInfo && cancelButton) {
+                editButton.addEventListener("click", function () {
+                    form.classList.toggle("d-none");
+                    playerInfo.classList.toggle("d-none");
+                });
+
+                cancelButton.addEventListener("click", function () {
+                    form.classList.add("d-none");
+                    playerInfo.classList.remove("d-none");
+                });
+            } else {
+                console.error(`Elements with data-id="${dataId}" not found.`);
+            }
+        });
     }
 
     toggleEditForm();
