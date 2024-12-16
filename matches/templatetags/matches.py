69f12376c6b643b8d7f5d5ccff58933333a4ca66
Denilson <fcodenilson@gmail.com>
2024-12-16 19:19:08 -0300

    Implement caching for match history to improve performance

--- matches/templatetags/matches.py
+++ matches/templatetags/matches.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.cache import cache
 from django.db.models import OuterRef, Subquery
 from django.template.defaultfilters import stringfilter
 from matches.models import Match
@@ -9,23 +10,30 @@ register = template.Library()
 
 @register.inclusion_tag("global/partials/list_matches.html", takes_context=True)
 def match_history(context):
-    mvp_subquery = Stat.objects.filter(team__match=OuterRef("pk"), mvp=True).values(
-        "player__username"
-    )[:1]
-    mvp_icon_subquery = Stat.objects.filter(
-        team__match=OuterRef("pk"), mvp=True
-    ).values("agent__icon")[:1]
+    if cache.get("matches"):
+        print("Cache hit")
+        matches = cache.get("matches")
+    else:
+        print("Cache miss")
+        mvp_subquery = Stat.objects.filter(team__match=OuterRef("pk"), mvp=True).values(
+            "player__username"
+        )[:1]
+        mvp_icon_subquery = Stat.objects.filter(
+            team__match=OuterRef("pk"), mvp=True
+        ).values("agent__icon")[:1]
 
-    matches = (
-        Match.objects.filter(is_finished=True)
-        .select_related("map", "winner")
-        .prefetch_related("teams__stats__player", "teams__players__main_agent")
-    )
+        matches = (
+            Match.objects.filter(is_finished=True)
+            .select_related("map", "winner")
+            .prefetch_related("teams__stats__player", "teams__players__main_agent")
+        )
 
-    matches = matches.annotate(
-        mvp=Subquery(mvp_subquery),
-        mvp_icon=Subquery(mvp_icon_subquery),
-    )
+        matches = matches.annotate(
+            mvp=Subquery(mvp_subquery),
+            mvp_icon=Subquery(mvp_icon_subquery),
+        )
+
+        cache.set("matches", matches)
 
     # if username is provided in the url, filter matches where the player is the mvp
     username = context["request"].resolver_match.kwargs.get("username")
