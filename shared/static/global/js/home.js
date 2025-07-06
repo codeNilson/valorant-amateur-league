eecd06e094ac3d3607cf7857fccdd20fa9cf204a
Denilson Silva <fcodenilson@gmail.com>
2025-07-06 07:33:07 -0300

    Update home.js

--- shared/static/global/js/home.js
+++ shared/static/global/js/home.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const updateButton = document.querySelector("#updateButton");
 
     updateButton.addEventListener("click", function (event) {
-        event.preventDefault(); // Impede o comportamento padrão inicialmente
+        event.preventDefault();
 
-        const confirmar = confirm("This will update the ranking and send a message in discord channel. Are you sure?");
-        if (confirmar) {
+        const confirmed = confirm("This will update the ranking and send a notification in discord channel. Proceed?");
+        if (confirmed) {
             event.target.closest("form").submit();
         }
     });
